e36aefd11c01f8218e97d450fe2cb090428e9a23
Caioalves14 <caio.alvesrex@gmail.com>
2023-07-31 11:23:08 -0300

    Spring Boot project

---
+++ src/test/java/com/caio_alves14/toDoSimple/ToDoSimpleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caio_alves14.toDoSimple;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoSimpleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
